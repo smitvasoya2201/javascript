220c9326dffebaeecf68a5ce5a0ec960267862db
smitvasoya2201 <vasoyasmit534@gmail.com>
2026-09-10 12:46:56 +0530

    first-task-js

---
+++ task1/task.js
@@ -0,0 +1,35 @@
+console.log("hello");
+document.write("main file");
+
+console.log(document.getElementById("h1"));
+console.log(document.getElementById("h1").innerHTML);
+
+console.log(document.getElementById("h2"));
+console.log(document.getElementById("h2").innerHTML);
+
+
+console.log(document.getElementById("h3"));
+console.log(document.getElementById("h3").innerHTML);
+
+
+console.log(document.getElementById("h4"));
+console.log(document.getElementById("h4").innerHTML);
+
+
+console.log(document.getElementById("h5"));
+console.log(document.getElementById("h5").innerHTML);
+
+
+console.log(document.getElementById("h6"));
+console.log(document.getElementById("h6").innerHTML);
+
+
+document.getElementById("h1").innerHTML = "helloh1";
+document.getElementById("h2").innerHTML = "helloh2";
+document.getElementById("h3").innerHTML = "helloh3";
+document.getElementById("h4").innerHTML = "helloh4";
+document.getElementById("h5").innerHTML = "helloh5";
+document.getElementById("h6").innerHTML = "helloh6";
+
+
+
